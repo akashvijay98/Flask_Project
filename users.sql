a2c9d182154c1ab779628188113f7611be7154b9
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-12-01 22:24:02 +0530

    Update users.sql

--- users.sql
+++ users.sql
@@ -1,2 +1,45 @@
+//for creating the table Users:
+
+
 create table Users(userid varchar(45),username char(45),password varchar(45));
  ALTER table Users add PRIMARY KEY(userid);
+                                               
+
+                                                                         
+//for creating a stored procedure:
+                                                                         
+                                                                         
+ DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `User`(
+    IN p_name VARCHAR(20),
+    IN p_username VARCHAR(20),
+    IN p_password VARCHAR(20)
+)
+BEGIN
+    if ( select exists (select 1 from Users where username = p_username) ) THEN
+     
+        select 'Username Exists !!';
+     
+    ELSE
+     
+        insert into Users
+        (
+            username,
+            userid,
+            password
+        )
+        values
+        (
+            p_name,
+            p_username,
+            p_password
+        );
+     
+    END IF;
+END$$
+DELIMITER ;                                                                      
+                                                                         
+                                                                         
+                                                                         
+                                                                         
+                                                                        
