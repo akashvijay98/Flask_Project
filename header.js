1ac48e37fbae91369ff2da4fdfd8389f14f96f53
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-12-06 22:53:12 +0530

    Create header.js

---
+++ header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import bobble from "./bobble.png"
+
+export default class Header extends React.Component {
+  render() {
+    return( 
+    
+    <div>
+  <h2> SIGNUP </h2>
+  
+    <img className="img-responsive logo" src={bobble} alt="" />
+    
+        
+          
+
+          </div>
+      );
+  }
+
+}
