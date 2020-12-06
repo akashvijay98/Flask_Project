4a2bd07c477570e6185869733f8c4c206e5c8633
akashvijay98 <31077544+akashvijay98@users.noreply.github.com>
2020-12-06 22:54:35 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./styles.css";
+import Header from "./component/Header";
+import Form from "./component/Form";
+
+export default function App() {
+  return (
+    <div className="App"> 
+       <Header/>
+
+        <Form/> 
+
+
+      
+      
+    
+    </div>
+      
+    
+  );
+}
